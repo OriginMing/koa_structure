c2ee4bf344ed6a6d5f64ab767b025869b1a46de2
王明 <2368102002@qq.com>
2021-10-12 13:14:46 +0800

    test

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router')
 const {create}  = require('../controller/user.controller')
 const userRouter = new Router({prefix:'/users'});
+console.log("hh");
 userRouter.post('/',create)
 module.exports = {
     userRouter
